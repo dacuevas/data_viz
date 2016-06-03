5dd7cbc8936c6c914c21643543a024130fc064b0
dacuevas <dcuevas08@gmail.com>
2016-06-03 11:52:11 -0700

    dc_libs.R: Initial commit.

---
+++ r/lib/dc_libs.R
@@ -0,0 +1,11 @@
+# dc_libs.R
+# Common R libraries used among most of my applications
+# Author: Daniel A. Cuevas
+# Last updated: 03 June 2016
+suppressMessages(require("ggplot2"))
+suppressMessages(require("scales"))
+suppressMessages(require("reshape2"))
+suppressMessages(require("getopt"))
+suppressMessages(require("ggthemes"))
+suppressMessages(require("plyr"))
+suppressMessages(require("grid"))
